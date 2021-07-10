4fbf6ba309d97a0450857b80948571a322a8cfb4
chrisdewa <62080903+chrisdewa@users.noreply.github.com>
2021-07-09 23:04:35 -0500

    engine comments

--- modules/database/engine.py
+++ modules/database/engine.py
@@ -4,4 +4,4 @@ __all__ = (
     'engine',
 )
 
-engine = AIOEngine(database='casechan') # engine runs on local database only for the moment.
+engine = AIOEngine(database='casechan')  # engine runs on local database only for the moment.
