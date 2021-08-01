3f8e3e928cdf613445f8f51d8bcaaec92a91b51d
owoqq <67983971+owoqq@users.noreply.github.com>
2021-08-01 15:27:57 +0300

    Added database switching for VM

--- modules/database/engine.py
+++ modules/database/engine.py
@@ -1,7 +1,10 @@
 from odmantic import AIOEngine
+import os
 
 __all__ = (
     'engine',
 )
 
-engine = AIOEngine(database='casechan')  # engine runs on local database only for the moment.
+DATABASE_URL = os.environ.get('DATABASE_URL')
+VM = os.environ.get('VM')
+engine = AIOEngine(database='casechan' if not VM else DATABASE_URL)  # engine runs on local database only for the moment.
