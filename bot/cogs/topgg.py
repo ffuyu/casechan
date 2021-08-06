b02cae2eca0970325d711454507e048511a07d1e
owoqq <67983971+owoqq@users.noreply.github.com>
2021-08-06 22:11:56 +0300

    top.gg implementation

--- bot/cogs/topgg.py
+++ bot/cogs/topgg.py
@@ -6,23 +6,16 @@ from ..bot import bot
 
 TOPGG_API = os.environ.get("TOPGG_API")
 
-bot.topggpy = topgg.DBLClient(bot, TOPGG_API)
-
-@tasks.loop(minutes=30)
-async def update_stats():
-    """This function runs every 30 minutes to automatically update your server count."""
-    try:
-        await bot.topggpy.post_guild_count()
-        print(f"Posted server count ({bot.topggpy.guild_count})")
-    except Exception as e:
-        print(f"Failed to post server count\n{e.__class__.__name__}: {e}")
-
-
-update_stats.start()
+bot.topggpy = topgg.DBLClient(bot, TOPGG_API, autopost=True, post_shard_count=True)
 
 bot.topgg_webhook = topgg.WebhookManager(bot).dbl_webhook("/dblwebhook", "password")
-
 bot.topgg_webhook.run(5000)
+
+@bot.event
+async def on_autopost_success():
+    print(
+        f"Posted server count ({bot.topggpy.guild_count}), shard count ({bot.shard_count})"
+    )
 
 
 @bot.event
