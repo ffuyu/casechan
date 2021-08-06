432e3526a01202cdf876357501ada9a061e8144a
owoqq <67983971+owoqq@users.noreply.github.com>
2021-08-06 22:18:49 +0300

    topgg.py is now a cog

--- bot/cogs/topgg.py
+++ bot/cogs/topgg.py
@@ -1,4 +1,4 @@
-from discord.ext import tasks
+from discord.ext import tasks, commands
 
 import topgg, os
 
@@ -11,23 +11,29 @@ bot.topggpy = topgg.DBLClient(bot, TOPGG_API, autopost=True, post_shard_count=Tr
 bot.topgg_webhook = topgg.WebhookManager(bot).dbl_webhook("/dblwebhook", "password")
 bot.topgg_webhook.run(5000)
 
-@bot.event
-async def on_autopost_success():
-    print(
-        f"Posted server count ({bot.topggpy.guild_count}), shard count ({bot.shard_count})"
-    )
+class TopGGCog(commands.Cog):
+    @bot.event
+    async def on_autopost_success():
+        print(
+            f"Posted server count ({bot.topggpy.guild_count}), shard count ({bot.shard_count})"
+        )
 
 
-@bot.event
-async def on_dbl_vote(data):
-    """An event that is called whenever someone votes for the bot on Top.gg."""
-    if data["type"] == "test":
-        return bot.dispatch("dbl_test", data)
+    @bot.event
+    async def on_dbl_vote(data):
+        """An event that is called whenever someone votes for the bot on Top.gg."""
+        if data["type"] == "test":
+            return bot.dispatch("dbl_test", data)
 
-    print(f"Received a vote:\n{data}")
+        print(f"Received a vote:\n{data}")
 
 
-@bot.event
-async def on_dbl_test(data):
-    """An event that is called whenever someone tests the webhook system for your bot on Top.gg."""
-    print(f"Received a test vote:\n{data}")
+    @bot.event
+    async def on_dbl_test(data):
+        """An event that is called whenever someone tests the webhook system for your bot on Top.gg."""
+        print(f"Received a test vote:\n{data}")
+
+
+
+def setup(bot):
+    bot.add_cog(TopGGCog(bot))
