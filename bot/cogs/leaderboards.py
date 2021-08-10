ef3c61f008d538f2aeff7150b634e37a1281addc
owoqq <67983971+owoqq@users.noreply.github.com>
2021-08-11 02:43:47 +0300

    Fixed sorting

--- bot/cogs/leaderboards.py
+++ bot/cogs/leaderboards.py
@@ -27,16 +27,17 @@ class LeaderboardsCog(commands.Cog, name='Leaderboards'):
     async def leaderboard(self, ctx: Context, *, guild: Optional[Guild]):
         """View the inventory worth leaderboard for the server"""
         guild = guild or ctx.guild
-        users = await engine.find(Player, Player.guild_id==guild.id, sort=Player.balance.desc(), limit=10)
+        users = await engine.find(Player, Player.guild_id==guild.id, limit=10)
         users_dictionary = {}
         for user in users:
             member = guild.get_member(user.member_id)
             if member:
                 users_dictionary[member.name] = await user.inv_total()
 
+        leaderboard = dict(sorted(users_dictionary.items(), key=lambda item: item[1], reverse=True))
         await ctx.send(
             embed=Embed(
-                description='\n'.join(f"**{k}**: ${v:.2f}" for k, v in users_dictionary.items()),
+                description='\n'.join(f"**{k}**: ${v:.2f}" for k, v in leaderboard.items()),
                 color=Colour.random()
             ).set_footer(text="Based on inventory worth | Total server inventory worth: ${:.2f}\nUse the command 'top' to view the world leaderboard.".format(
                 sum([x for x in users_dictionary.values()]))).set_author(name=guild, icon_url=guild.icon_url)
