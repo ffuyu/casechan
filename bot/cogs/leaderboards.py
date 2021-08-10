cf61fa7d10ea3efd42bc67f1529b34957baa48a5
owoqq <67983971+owoqq@users.noreply.github.com>
2021-08-11 02:33:00 +0300

    Added format specifiers for balances shown in leaderboard

--- bot/cogs/leaderboards.py
+++ bot/cogs/leaderboards.py
@@ -37,7 +37,7 @@ class LeaderboardsCog(commands.Cog, name='Leaderboards'):
         leaderboard = dict(sorted(users_dictionary.items(), key=lambda item: item[1], reverse=True))
         await ctx.send(
             embed=Embed(
-                description='\n'.join(f"**{k}**: ${v}" for k, v in leaderboard.items()),
+                description='\n'.join(f"**{k}**: ${v:.2f}" for k, v in leaderboard.items()),
                 color=Colour.random()
             ).set_footer(text="Based on inventory worth | Total server inventory worth: ${:.2f}\nUse the command 'top' to view the world leaderboard.".format(
                 sum([x for x in users_dictionary.values()]))).set_author(name=guild, icon_url=guild.icon_url)
