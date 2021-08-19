4f401752137d2f4c5d012af097c4a0a90a3298aa
owoqq <67983971+owoqq@users.noreply.github.com>
2021-08-19 21:47:19 +0300

    emojis.py contants are now using Enum

--- etc/emojis.py
+++ etc/emojis.py
@@ -1,21 +1,25 @@
-BASE_GRADE = "<:basegrade:874985683029225542>"
-INDUSTRIAL_GRADE = "<:industrialgrade:874985682525884417>"
-CONSUMER_GRADE = "<:consumergrade:874985682932756480>"
-MILSPEC_GRADE = "<:milspec:874985682790133771>"
-RESTRICTED = "<:restricted:874985683175997461>"
-CLASSIFIED = "<:classified:874985683066949672>"
-COVERT = "<:covert:874985682970488872>"
-CONTRABAND = "<:contraband:874985682924339270>"
+from enum import Enum
+
+class Emojis(Enum):
+    BASE_GRADE = "<:basegrade:874985683029225542>"
+    INDUSTRIAL_GRADE = "<:industrialgrade:874985682525884417>"
+    CONSUMER_GRADE = "<:consumergrade:874985682932756480>"
+    MILSPEC_GRADE = "<:milspec:874985682790133771>"
+    RESTRICTED = "<:restricted:874985683175997461>"
+    CLASSIFIED = "<:classified:874985683066949672>"
+    COVERT = "<:covert:874985682970488872>"
+    CONTRABAND = "<:contraband:874985682924339270>"
+
 
 emojis = {
-    "Base Grade": BASE_GRADE,
-    "Industrial Grade": INDUSTRIAL_GRADE,
-    "Consumer Grade": CONSUMER_GRADE,
-    "Mil-Spec Grade": MILSPEC_GRADE,
-    "Restricted": RESTRICTED,
-    "Classified": CLASSIFIED,
-    "Covert": COVERT,
-    "Extraordinary": COVERT,
-    "Exceedingly Rare Item": COVERT,
-    "Contraband": CONTRABAND,
+    "Base Grade": Emojis.BASE_GRADE.value,
+    "Industrial Grade": Emojis.INDUSTRIAL_GRADE.value,
+    "Consumer Grade": Emojis.CONSUMER_GRADE.value,
+    "Mil-Spec Grade": Emojis.MILSPEC_GRADE.value,
+    "Restricted": Emojis.RESTRICTED.value,
+    "Classified": Emojis.CLASSIFIED.value,
+    "Covert": Emojis.COVERT.value,
+    "Extraordinary": Emojis.COVERT.value,
+    "Exceedingly Rare Item": Emojis.COVERT.value,
+    "Contraband": Emojis.CONTRABAND.value,
 }
